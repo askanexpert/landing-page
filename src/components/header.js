c54d9b776a875f7b396b49a97b6183b54755fdcd
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-06-08 10:33:32 +0530

    added main splash and logo

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,8 @@ class Header extends Component {
   render() {
     return (
       <div className='aae-section__container header'>
-        <img id='logo-image' src='../../resources/logo.png'/>
+        <img id='logo-image' src='../../style/images/logo.png'/>
+        <img id='main-splash' src='../../style/images/main-splash.png'/>
       </div>
     );
   }
