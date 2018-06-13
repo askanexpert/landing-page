306fe97674937c008235f080677c83bb332bf756
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-06-13 19:14:28 +0530

    bug squash 1.7

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
-var ROOT_SERVER_URL = 'http://localhost:3000';
-if(process.env.NODE_ENV == 'production') {
-  ROOT_SERVER_URL = 'http://askanexpert-server.herokuapp.com';
+var ROOT_SERVER_URL = 'http://askanexpert-server.herokuapp.com';
+if(process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'test') {
+  ROOT_SERVER_URL = 'http://localhost:3000';
 }
 
 export default ROOT_SERVER_URL;
