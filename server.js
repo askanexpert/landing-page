b121a3f500c77d43b4089cb3ad7f7b9ceb24728c
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-06-13 19:49:07 +0530

    revert

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './../../', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 })
 
 app.listen(port, () => {
