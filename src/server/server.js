5ef16c450808844700a5a22bf7a3a84cfc62e4df
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-06-13 18:43:50 +0530

    Added Sample Server for Landing Page

---
+++ src/server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Express middleware to convert request body to json
+app.use(bodyParser.json());
+
+// POST /leads to endpoint - Used to add a lead to the database
+app.get('/', (req, res) => {
+  res.status(200).send('Landing Page Active...')
+});
+
+app.listen(port, () => {
+  console.log(`Started listening on port ${port}`);
+})
