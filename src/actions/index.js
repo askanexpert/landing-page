f3866dbc5f5c75e501781aa1341808d9b8f5dc82
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-06-13 19:17:59 +0530

    bug squash

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import * as CONFIG_VARS from '../config/config';
+import ROOT_SERVER_URL from '../config/config';
 
 export const ACTION_SIGNUP = 'SIGNUP';
 
 export function signup(email) {
   const request = axios.post(
-    `${CONFIG_VARS.ROOT_SERVER_URL}/leads`, {email});
+    `${ROOT_SERVER_URL}/leads`, {email});
 
   return (dispatch) => {
     request.then((data) => {
