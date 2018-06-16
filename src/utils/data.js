01ae091cd70410887a8ab1ad3466ba970b3a8dbc
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-06-17 02:50:00 +0530

    Added compressed images

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,7 +1,7 @@
 export const headerData = {
-  logoImage: 'custom/header/logo.png',
+  logoImage: 'custom/header/logo-min.png',
   title: 'Decentralised real-time chat with experts',
-  mainSplashImage: 'custom/header/main_splash.png'
+  mainSplashImage: 'custom/header/main_splash-min.png'
 }
 export const footerData = {
   title: 'Thanks for submitting your response',
@@ -9,27 +9,27 @@ export const footerData = {
 }
 export const sectionData = [
   {
-    image: 'custom/sections/expert_explorer.png',
+    image: 'custom/sections/expert_explorer-min.png',
     title: 'Discover the best Topic Specific Experts',
     description: `Discover numerous top notch industry leading experts based on
     topics you choose. See relevant details, social media profiles and make
     an informed decision.`
   },
   {
-    image: 'custom/sections/expert_scheduler.png',
+    image: 'custom/sections/expert_scheduler-min.png',
     title: 'Schedule timed Chat Sessions',
     description: `Easy to schedule flexible chat sessions. Schedule 10 minute,
     30 minute or 1 hour sessions based on your convenience and requirement.`
   },
   {
-    image: 'custom/sections/expert_chat_ui.png',
+    image: 'custom/sections/expert_chat_ui-min.png',
     title: 'Own your data with decentralised storage',
     description: `With decentralised storage, we provide you with an open data
     graph of your interactions that you can port anywhere. You will get each
     session transcript emailed to you as well.`
   },
   {
-    image: 'custom/sections/expert_chat_storage.png',
+    image: 'custom/sections/expert_chat_storage-min.png',
     title: 'Earn money with your data',
     description: `Earn money when third parties request access to your chat
     session data using our Data API. Chat session data is processed before sending
