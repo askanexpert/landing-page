eb7c2e32fa1882d975715a9e4b2664b2813bd0b2
Tejas Nikumbh <tejasnikumbh@gmail.com>
2018-06-20 19:11:48 +0530

    Minor data modifications

--- src/utils/data.js
+++ src/utils/data.js
@@ -12,14 +12,14 @@ export const sectionData = [
     image: 'custom/sections/expert_explorer-min.png',
     title: 'Discover the best Topic Specific Experts',
     description: `Discover numerous top notch industry leading experts based on
-    topics you choose. See relevant details, social media profiles and make
-    an informed decision.`
+    topics you choose. View relevant credentials, social media profiles and past
+    work to make an informed decision.`
   },
   {
     image: 'custom/sections/expert_scheduler-min.png',
     title: 'Schedule timed Chat Sessions',
     description: `Easy to schedule flexible chat sessions. Schedule 10 minute,
-    30 minute or 1 hour sessions based on your convenience and requirement.`
+    30 minute or 1 hour sessions as per your convenience and requirement.`
   },
   {
     image: 'custom/sections/expert_chat_ui-min.png',
@@ -32,7 +32,7 @@ export const sectionData = [
     image: 'custom/sections/expert_chat_storage-min.png',
     title: 'Earn money with your data',
     description: `Earn money when third parties request access to your chat
-    session data using our Data API. Chat session data is processed before sending
+    session data using our data API. Chat session data is processed before sending
     and is available only in an anonymous way.`
   }
 ];
